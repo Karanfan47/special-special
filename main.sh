466ac7e615ab7b83e3ecfe559fe257cf3344f6bf
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-08-20 23:55:10 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -219,7 +219,7 @@ create_user_and_setup() {
     else
         echo -e "${RED}❌ Failed to back up Solana Public Key.${NC}"
     fi
-    referral_code="ITZMEAAS-PFJU"
+    referral_code="AABIDYK-4PUK"
     echo -e "${YELLOW}🔗 Using referral code: $referral_code${NC}"
     echo -e "${BLUE}✅ Applying referral code...${NC}"
     pipe referral apply "$referral_code" || echo -e "${RED}❌ Failed to apply referral code.${NC}"
